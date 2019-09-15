86733194cef0ed6e27bacfcc4f1760bc25141425
Dakota Grusak <dgrusak@trinity.edu>
2019-09-15 11:02:59 -0500

    Add LP3THW folder and  excercises
    Add Learn Python 3 The Hard Way folder with current worked excercises

---
+++ learn-python-3-the-hard-way/ex2.py
@@ -0,0 +1,13 @@
+# A commen, this is so you can read your program later
+# Anything after the # is ignored in python.
+
+print ("I could have code like this.") # and the comment after is ignored
+
+# You can also commen to "disable" or comment out a piece of code:
+# print "This won't run."
+
+print ("This will run.")
+
+
+# Excercise 2
+# Comment and Pound Character
